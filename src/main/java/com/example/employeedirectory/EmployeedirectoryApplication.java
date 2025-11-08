b86f10aec3fc3b0a98c8e56f68ab6d8ffd550c6f
Takwa-Laffet <laffet@kebili.r-iset.tn>
2025-11-08 22:03:29 +0100

    fix

--- src/main/java/com/example/employeedirectory/EmployeedirectoryApplication.java
+++ src/main/java/com/example/employeedirectory/EmployeedirectoryApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmployeedirectoryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmployeedirectoryApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeedirectoryApplication.class, args);
+    }
 }
--- src/test/java/com/example/employeedirectory/EmployeedirectoryApplicationTests.java
+++ src/test/java/com/example/employeedirectory/EmployeedirectoryApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EmployeedirectoryApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        // Test juste pour charger le contexte
+    }
 }
